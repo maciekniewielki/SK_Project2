957101ab3377bd03a8fc53056496efbb20abb1d8
Maciej Buczyński <kadlubken5@gmail.com>
2017-05-22 23:39:25 +0200

    First attempt for multiple clients

--- Server/src/libs/user_connection.py
+++ Server/src/libs/user_connection.py
@@ -1,2 +1,55 @@
 import socket
+import threading
+import Server.src.libs.constants as constants
+import Server.src.server as server
+from Server.src.libs.errors import NameTaken, ValidationException
 
+
+class ClientConnection(threading.Thread):
+    lock = threading.Lock()
+
+    def __init__(self, connection):
+        super().__init__()
+        self.connection = connection
+
+    def send_string(self, s):
+        self.connection.send(s.encode())
+
+    def run(self):       # TODO maciekniewielki finish
+        logged_in = 0
+        login = ""
+
+        while not logged_in:
+            data = self.connection.recv(constants.BUFFER_SIZE).decode()
+            if not data.count(" ") == 2:
+                print("Bad data")
+                self.send_string("0 %s" % "Bad data formatting. Do not use spaces in login or password")
+                continue
+            option, login, password = data.split(" ")
+            print("option %s, login %s, password %s" % (option, login, password))
+            if option == "r":
+                try:
+                    with ClientConnection.lock:
+                        server.register_user(login, password)
+
+                except (ValidationException, NameTaken) as e:
+                    self.send_string("0 %s" % e.value)
+                    continue
+                self.send_string("1 You have been registered and logged in. Your highscore is 0")
+                logged_in = 1
+            elif option == "l":
+                print(server.user_data)
+                if login in server.user_data:
+                    with ClientConnection.lock:
+                        result = server.check_password(login, password)
+                    if result:
+                        logged_in = 1
+                        self.send_string("1 Welcome %s! Your highscore is %s" % (login, server.user_data[login][1]))
+                    else:
+                        self.send_string("0  Wrong password for user %s" % login)
+                        continue
+                else:
+                    self.send_string("0  No user %s in database" % login)
+                    continue
+        print("%s has logged in" % login)
+        self.connection.close()
--- Server/src/server.py
+++ Server/src/server.py
@@ -3,100 +3,69 @@ import hashlib      # for hashing passwords
 import re           # for checking if nicks and passwords are valid
 
 from Server.src.libs.errors import NameTaken, ValidationException
+import Server.src.libs.constants as constants
+import Server.src.libs.user_connection
 
-data_file = "user_data.dat"
-user_data = []
-
-server_ip = "127.0.0.1"
-server_port = 5005
-BUFFER_SIZE = 1024
+user_data = {}
+client_threads = []
 
 
-def wait_for_connection():
+def wait_for_connection():  # TODO maciekniewielki add maximum number of connections
+    global client_threads
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind((server_ip, server_port))
+    s.bind((constants.SERVER_IP, constants.SERVER_PORT))
     s.listen(1)
 
-    conn, addr = s.accept()
-    print("Connection from", addr)
-    connection(conn)
+    while True:
+        conn, addr = s.accept()
+        print("Connection from", addr)
+        user_thread = Server.src.libs.user_connection.ClientConnection(conn)
+        user_thread.run()
+        if client_threads:
+            client_threads = [x for x in client_threads if x.is_alive()] + [user_thread]
+        else:
+            client_threads = [user_thread]
 
 
-def connection(conn):       #TODO maciekniewielki finish
-    logged_in = 0
-    login = ""
-
-    def send_string(s):
-        conn.send(s.encode())
-
-    while not logged_in:
-        data = conn.recv(BUFFER_SIZE).decode()
-        if not data.count(" ") == 2:
-            print("Bad data")
-            send_string("0 %s" % "Bad data formatting. Do not use spaces in login or password")
-            continue
-        option, login, password = data.split(" ")
-        print("option %s, login %s, password %s", option, login, password)
-        if option == "r":
-            try:
-                register_user(login, password)
-            except (ValidationException, NameTaken) as e:
-                send_string("0 %s" % e.value)
-                continue
-            send_string("1 You have been registered and logged in. Your highscore is 0")
-            logged_in = 1
-        elif option == "l":
-            user = [user for user in user_data if user[0] == login]
-            if user:
-                if check_password(login, password):
-                    logged_in = 1
-                    print(user)
-                    send_string("1 Welcome %s! Your highscore is %s" % (login, user[0][2]))
-                else:
-                    send_string("0  Wrong password for user %s" % login)
-                    continue
-            else:
-                send_string("0  No user %s in database" % login)
-                continue
-    print("%s has logged in" % login)
-    conn.close()
-
-
-def start_server():     # TODO maciekniewielki init user connections, open ports
+def start_server():
+    global user_data
     try:
-        with open(data_file, "r") as file:
+        with open(constants.DATA_FILE, "r") as file:
             for line in file:
-                user_data.append(line[:-1].split(" "))
+                login, hashed_password, highscore = line[:-1].split(" ")
+                user_data[login] = (hashed_password, int(highscore))
     except FileNotFoundError:
-        open(data_file, "w").close()
-
+        open(constants.DATA_FILE, "w").close()
     wait_for_connection()
 
 
 def _write_user_(user):
-    with open(data_file, "a") as file:
+    with open(constants.DATA_FILE, "a") as file:
         line = [str(x) for x in user]
         file.write(" ".join(line) + "\n")
 
 
 def register_user(nick, password):
+    global user_data
     if not re.fullmatch(r'[A-Za-z0-9_]+', nick):
         raise ValidationException("BAD_LOGIN")
     if not re.fullmatch(r'[A-Za-z0-9_!@#$%^&]+', password):
         raise ValidationException("BAD_PASSWORD")
-    if any(x[0] == nick for x in user_data):
+    if nick in user_data:
         raise NameTaken("This name is already taken")
 
     hashed_password = hashlib.sha512(password.encode()).hexdigest()
+    user_data[nick] = (hashed_password, 0)
     user = nick, hashed_password, 0
-    user_data.append(user)
     _write_user_(user)
     print("Registered user", user)
+    print(user_data)
 
 
 def check_password(nick, password):
+    global user_data
     hashed_password = hashlib.sha512(password.encode()).hexdigest()
-    if len([user for user in user_data if user[1] == hashed_password]):
+    if nick in user_data and user_data[nick][0] == hashed_password:
         return True
     else:
         return False
